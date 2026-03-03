c1c925bfb5f5a7bc9fe08110ee95e4cf26ce2b3c
Jacob Sussmilch <suisuss@protonmail.com>
2026-03-03 14:30:20 +1100

    refactor: extract isRemoteMode to shared util

--- tests/e2e/playwright/global-teardown.ts
+++ tests/e2e/playwright/global-teardown.ts
@@ -1,14 +1,11 @@
 import dotenv from "dotenv";
 import { expand } from "dotenv-expand";
 import { cleanupTestUsers } from "./utils/cleanup";
+import { isRemoteMode } from "./utils/env";
 import { cleanupPersistentTestUsers } from "./utils/seed";
 
 const DEFAULT_DB_URL =
   "postgresql://postgres:postgres@localhost:5433/keeperhub";
-
-function isRemoteMode(): boolean {
-  return !!(process.env.BASE_URL && process.env.TEST_API_KEY);
-}
 
 async function globalTeardown(): Promise<void> {
   expand(dotenv.config());
