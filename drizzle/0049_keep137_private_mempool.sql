e3f7681ca3e7361e6a8f8c0ffa12ede48d9f0a84
Jacob Sussmilch <suisuss@protonmail.com>
2026-04-17 16:20:05 +1000

    Revert "fix: KEEP-137 seed ETH mainnet Flashbots URL in migration"
    This reverts commit befe72a8dd7537fe50945a654ab384dbe5587d38.

--- drizzle/0049_keep137_private_mempool.sql
+++ drizzle/0049_keep137_private_mempool.sql
@@ -1,3 +1,2 @@
 ALTER TABLE "chains" ADD COLUMN "use_private_mempool_rpc" boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE "chains" ADD COLUMN "default_private_rpc_url" text;--> statement-breakpoint
-UPDATE "chains" SET "use_private_mempool_rpc" = true, "default_private_rpc_url" = 'https://rpc.flashbots.net/fast' WHERE "chain_id" = 1;
+ALTER TABLE "chains" ADD COLUMN "default_private_rpc_url" text;
